c96632814236a344d7b9ddeb93cc1e5e6e5e2113
SakshamKhanuja <ssk@outlook.ie>
2022-07-27 13:41:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Stores the number of pounds.
+        double pounds = 247D;
+
+        // Stores the number of kilograms equivalent to those in variable "pounds".
+        double poundsConvertedToKilograms = pounds * 0.45359237;
+
+        // Prints the conversion.
+        System.out.println(pounds + " pounds = " + poundsConvertedToKilograms + " kilograms");
+    }
+}
